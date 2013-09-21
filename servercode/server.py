fad4acebae162c271cf26afb0ed9fc9febd5f5a2
Riizade <riizade@gmail.com>
2013-09-21 08:10:13 -0400

    Added game-grabbing functionality through Steam API

--- servercode/server.py
+++ servercode/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import urllib2
+import json
 import re
 app = Flask(__name__)
 
@@ -17,6 +18,16 @@ def get_community_id(steam_community_url):
 	community_id = search_results.group(0)
 	community_id = community_id.rstrip('"')
 	return community_id
+
+def get_owned_games(apikey, community_id):
+	api_url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + apikey + "&steamid=" + community_id + "&format=json"
+	response = urllib2.urlopen(api_url)
+	json_games = response.read()
+	games = json.JSONDecoder().decode(json_games)
+	games_list = []
+	for game in games['response']['games']:
+		games_list.append([game['appid'], game['playtime_forever']])
+	return games_list
 
 
 if __name__ == '__main__':
