c4bc61d8fc84c32c5f4823ee2585d22a6f8d1b81
Riizade <riizade@gmail.com>
2013-09-21 08:33:57 -0400

    Refactored game-grabbing to return a dictionary with appid keys and playtime_forever values

--- servercode/server.py
+++ servercode/server.py
@@ -11,6 +11,7 @@ def hello_world():
     return 'Hello World!'
 
 def get_community_id(steam_community_url):
+	#returns JUST the number representing a user's community id
 	def get_community_id(steam_community_url):
 	response = urllib2.urlopen(steam_community_url)
 	html = response.read()
@@ -20,14 +21,15 @@ def get_community_id(steam_community_url):
 	return community_id
 
 def get_owned_games(apikey, community_id):
+	#returns a dictionary of appid keys and playtime_forever values for the profile
 	api_url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + apikey + "&steamid=" + community_id + "&format=json"
 	response = urllib2.urlopen(api_url)
 	json_games = response.read()
 	games = json.JSONDecoder().decode(json_games)
-	games_list = []
+	games_dict = {}
 	for game in games['response']['games']:
-		games_list.append([game['appid'], game['playtime_forever']])
-	return games_list
+		games_dict[game['appid']] = game['playtime_forever']
+	return games_dict
 
 
 if __name__ == '__main__':
